4f652d1ff5fd2c81277a5a84bbe42a3657634234
Congb19 <347006087@qq.com>
2020-06-01 23:32:31 +0800

    6.1

--- scope-react/src/App.js
+++ scope-react/src/App.js
@@ -3,93 +3,53 @@ import yolo from "tfjs-yolo";
 
 export default class App extends Component {
 	async yolo3() {
+		// 开始
+		// todo：1 拍照作为img dom元素  2
 		console.log("begin");
+		const video = document.getElementById("video"),
+			canvas = document.getElementById("canvas"),
+			img = document.getElementById("img"),
+			vendorUrl = window.URL || window.webkitURL;
+		//媒体对象
+		navigator.getMedia =
+			navigator.getUserMedia ||
+			navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia ||
+			navigator.msGetUserMedia;
+		var constraints = { audio: false, video: { width: 783, height: 390 } };
+		navigator.mediaDevices
+			.getUserMedia(constraints)
+			.then(function (mediaStream) {
+				var video = document.querySelector("video");
+				video.srcObject = mediaStream;
+				video.onloadedmetadata = function (e) {
+					video.play();
+				};
+			})
+			.catch(function (err) {
+				console.log(err.name + ": " + err.message);
+			});
+		canvas.getContext("2d").drawImage(video, 0, 0, 400, 300);
+		img.src = canvas.toDataURL("image/png");
+
+		// PART 2 识别 img=要识别的图
 		const $img = document.getElementById("img");
 		let myYolo = await yolo.v3(
 			"http://192.168.123.222:8080/yolov3-tiny/model.json"
 		);
 		const boxes = await myYolo.predict($img);
+
+		// PART 3 结果处理 boxes=识别结果
+		// todo：1 在底座上渲染模型（maybe可交互旋转什么的）  2 弹出相关信息，文字，百科链接
 		console.log(boxes);
 	}
 	render() {
 		return (
-			<a-scene
-				embedded
-				arjs="sourceType: webcam;"
-				renderer="antialias: true; alpha: true; precision: medium;"
-			>
-				<a-marker preset="hiro">
-					<a-sphere
-						position="0 0.5 0"
-						radius="0.5"
-						color="#EF2D5E"
-					></a-sphere>
-					<a-plane
-						position="0 0 0"
-						rotation="-90 0 0"
-						width="1"
-						height="1"
-						color="#7BC8A4"
-					></a-plane>
-				</a-marker>
-				<a-entity camera></a-entity>
-			</a-scene>
-			// <a-scene>
-			// 	<a-box
-			// 		position="-1 0.5 -3"
-			// 		rotation="0 45 0"
-			// 		color="#4CC3D9"
-			// 	></a-box>
-			// 	<a-plane
-			// 		position="0 0 -4"
-			// 		rotation="-90 0 0"
-			// 		width="4"
-			// 		height="4"
-			// 		color="#7BC8A4"
-			// 	></a-plane>
-			// </a-scene>
-			// <a-scene
-			// 	vr-mode-ui="enabled: false;"
-			// 	renderer="logarithmicDepthBuffer: true;"
-			// 	embedded
-			// 	arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
-			// >
-			// 	<a-nft
-			// 		type="nft"
-			// 		url="trex/trex-image/K"
-			// 		smooth="true"
-			// 		smoothCount="10"
-			// 		smoothTolerance=".01"
-			// 		smoothThreshold="5"
-			// 	>
-			// 		<a-box
-			// 			position="-1 0.5 -3"
-			// 			rotation="0 45 0"
-			// 			color="#4CC3D9"
-			// 		></a-box>
-			// 		<a-sphere
-			// 			position="0 1.25 -5"
-			// 			radius="1.25"
-			// 			color="#EF2D5E"
-			// 		></a-sphere>
-			// 		<a-cylinder
-			// 			position="1 0.75 -3"
-			// 			radius="0.5"
-			// 			height="1.5"
-			// 			color="#FFC65D"
-			// 		></a-cylinder>
-			// 		<a-plane
-			// 			position="0 0 -4"
-			// 			rotation="-90 0 0"
-			// 			width="4"
-			// 			height="4"
-			// 			color="#7BC8A4"
-			// 		></a-plane>
-			// 		<a-sky color="#ECECEC"></a-sky>
-			// 	</a-nft>
-			// 	<a-entity camera></a-entity>
-			// </a-scene>
+			<button id="check" class="btn btn-check" onClick={this.yolo3}>
+				check!
+			</button>
 		);
-		// return <div onClick={this.yolo3}>aa</div>;
 	}
 }
+// let aa = document.getElementById("aa");
+// aa.click();
